9f8490c1652f45ec60e5e10013de428aa8198af2
Ruan <ti06@sollucao.net>
2023-08-21 13:25:29 -0300

    ajustes no botão de cadastrar

--- src/components/Cadastro/Cadastro.tsx
+++ src/components/Cadastro/Cadastro.tsx
@@ -25,7 +25,7 @@ async function handleSubmit(data: PropsCadastro) {
             password: data.senha,
             password_confirmation: data.confirmaSenha
         });
-        
+
         console.log(`Usuário registrado: ${response.data}`)
     } catch (error) {
         console.error(error);
@@ -35,16 +35,16 @@ async function handleSubmit(data: PropsCadastro) {
 
 const usuarioSchema = yup.object().shape({
     nomeSobrenome: yup.string().required('Por favor, digite seu nome completo').test(
-        'testa-nome-sobrenome', 
-        'Por favor, digite seu nome e sobrenome', 
+        'testa-nome-sobrenome',
+        'Por favor, digite seu nome e sobrenome',
         (value) => {
             const nomeSobrenome = value.split(' ').filter((nomeSobrenome) => nomeSobrenome.trim() != '');
             return nomeSobrenome.length >= 2
         }),
     email: yup.string().email('Por favor, digite um e-mail válido').required('Por favor, digite seu e-mail'),
     senha: yup.string().min(8, 'A senha deve ter no mínimo 8 caracteres')
-            .matches(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-            .matches(/\W+/gm, 'A senha deve conter pelo menos um carácter especial'),
+        .matches(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+        .matches(/\W+/gm, 'A senha deve conter pelo menos um carácter especial'),
     confirmaSenha: yup.string().test(
         'testa-senhas-iguais',
         'Por favor, verifique sua senha',
@@ -66,71 +66,77 @@ export function Cadastro() {
                 <p className="form-text">para iniciar</p>
                 <Formik<PropsCadastro>
                     initialValues={{
-                        nomeSobrenome: '', 
-                        email: '', 
-                        senha: '', 
-                        confirmaSenha: '', 
+                        nomeSobrenome: '',
+                        email: '',
+                        senha: '',
+                        confirmaSenha: '',
                         aceiteTermos: false
                     }}
                     onSubmit={handleSubmit}
                     validateOnChange={false}
                     validateOnBlur={false}
                     validationSchema={usuarioSchema}
-                    >
+                >
 
-                    {({setFieldValue, errors, touched}) => (
+                    {({ setFieldValue, setFieldTouched, errors, touched, isValid }) => (
+
                         <Form className="form-content">
-                        <Input 
-                            id='nomeSobrenome'
-                            name='nomeSobrenome'
-                            type='text'
-                            placeholder='Nome e Sobrenome'
-                            imgSrc={userIcon}
-                            erros={touched.nomeSobrenome && errors.nomeSobrenome}
-                        />
-                        <Input
-                            id='email'
-                            name='email'
-                            type='email'
-                            placeholder='E-mail'
-                            imgSrc={emailIcon}
-                            erros={touched.email && errors.email}
-                        />
-                        <Input
-                            id='senha'
-                            name='senha'
-                            type='password'
-                            placeholder='Senha'
-                            imgSrc={senhaIcon}
-                            erros={touched.senha && errors.senha}
-                        />
-                        <Input
-                            id='confirmaSenha'
-                            name='confirmaSenha'
-                            type='password'
-                            placeholder='Confirma senha'
-                            imgSrc={senhaIcon}
-                            erros={touched.confirmaSenha && errors.confirmaSenha}
-                        />
-                        <div className='form-checkbox'>
-                            <Field 
-                                id="html"
-                                type="checkbox" 
-                                name='aceiteTermos'
-                                checked={aceiteTermos}
-                                onChange={() => {
-                                    setFieldValue('aceiteTermos', !aceiteTermos);
-                                    setAceiteTermos(!aceiteTermos);
-                                }}
+                            <Input
+                                id='nomeSobrenome'
+                                name='nomeSobrenome'
+                                type='text'
+                                placeholder='Nome e Sobrenome'
+                                imgSrc={userIcon}
+                                erros={touched.nomeSobrenome && errors.nomeSobrenome}
                             />
-                            <label htmlFor="html"></label>
-                            <span className='form-checkbox-text'>
-                                Declaro que li e concordo com os termos e condições de uso.
-                            </span>
-                        </div>
-                        <Button>Cadastrar</Button>
-                        <span className="cadastro-span">Voltar</span>
-                    </Form>
+                            <Input
+                                id='email'
+                                name='email'
+                                type='email'
+                                placeholder='E-mail'
+                                imgSrc={emailIcon}
+                                erros={touched.email && errors.email}
+                            />
+                            <Input
+                                id='senha'
+                                name='senha'
+                                type='password'
+                                placeholder='Senha'
+                                imgSrc={senhaIcon}
+                                erros={touched.senha && errors.senha}
+                            />
+                            <Input
+                                id='confirmaSenha'
+                                name='confirmaSenha'
+                                type='password'
+                                placeholder='Confirma senha'
+                                imgSrc={senhaIcon}
+                                erros={touched.confirmaSenha && errors.confirmaSenha}
+                            />
+                            <div className='form-checkbox'>
+                                <Field
+                                    id="html"
+                                    type="checkbox"
+                                    name='aceiteTermos'
+                                    checked={aceiteTermos}
+                                    onChange={() => {
+                                        setFieldValue('aceiteTermos', !aceiteTermos);
+                                        setAceiteTermos(!aceiteTermos);
+                                    }}
+                                />
+                                <label htmlFor="html"></label>
+                                <span className='form-checkbox-text'>
+                                    Declaro que li e concordo com os termos e condições de uso.
+                                </span>
+                            </div>
+                            <Button style={{ backgroundColor: isValid && Object.keys(touched).length === 4 && !Object.values(errors).some(error => !!error) ? '#0575E6' : '#BBB' }}
+                                onClick={() => {
+                                    Object.keys(touched).forEach(field => {
+                                        setFieldTouched(field, true);
+                                    });
+                                }}>Cadastrar</Button>
+                            <span className="cadastro-span">Voltar</span>
+                        </Form>
                     )}
                 </Formik>
             </div>
